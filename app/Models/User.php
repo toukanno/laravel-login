ea0f67e461871502e40a637cb266e87f12e3cdd4
masayoshiyuto <y-masayoshi@kazehikaru.co.jp>
2023-04-02 21:45:10 +0900

    ビジネスロジックを分離しよう

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,60 @@ class User extends Authenticatable
     // protected $casts = [
     //     'email_verified_at' => 'datetime',
     // ];
+
+    /**
+     * Emailがマッチしたユーザを返す
+     * @param string $email
+     * @return object
+     */
+    public function getUserByEmail($email){
+        return User::where('email','=',$email)->first();
+    }
+
+    /**
+     * アカウントがロックされているか？
+     * @param object $user
+     * @return bool
+     */
+    public function isAccountLocked($user)
+    {
+        if($user->locked_flg ===1) {
+            return true;
+        }
+        return false;
+
+    }
+    /**
+     * エラーカウントをリセットする
+     * @param object $user
+     */
+    public function resetErrorCount($user){
+        if($user->error_count > 0) {
+            $user->error_count = 0;
+            $user->save();
+        }
+
+    }
+
+    /**
+     * エラーカウントを1増やす
+     * @param int $error_count
+     * @param int
+     */
+    public function addErrorCount($error_count){
+        return $error_count + 1;
+    }
+    /**
+     * アカウントをロックする
+     * @param object $user
+     * @return bool
+     */
+    public function lockAccount($user)
+    {
+        if($user->error_count > 5) {
+            $user->locked_flg = 1;
+            return $user->save();
+        }
+        return false;
+    }
 }
